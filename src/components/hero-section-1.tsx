fe7ea2cd6a6d62cfdd1692b6cef6f5b16d013712
Sten-Jan Sarv <42585583+stenjansarv@users.noreply.github.com>
2025-10-31 19:27:51 +0200

    fix: add some space above the reval title

--- src/components/hero-section-1.tsx
+++ src/components/hero-section-1.tsx
@@ -216,13 +216,13 @@ export function HeroSection() {
                     <div className="h-[80rem] -translate-y-[350px] absolute left-0 top-0 w-56 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
                 </div>
                 <section>
-                    <div className="relative pt-16 md:pt-24">
+                    <div className="relative pt-20 md:pt-36">
                         <div aria-hidden className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--background)_75%)]" />
                         <div className="mx-auto max-w-7xl px-6">
                             <div className="text-center sm:mx-auto lg:mr-auto lg:mt-0">
                                 <AnimatedGroup variants={transitionVariants}>
                                     <h1
-                                        className="mt-2 max-w-4xl mx-auto text-balance text-6xl md:text-7xl lg:mt-4 xl:text-[5.25rem]">
+                                        className="mt-4 max-w-4xl mx-auto text-balance text-6xl md:text-7xl lg:mt-6 xl:text-[5.25rem]">
                                         REVAL
                                     </h1>
                                 </AnimatedGroup>
